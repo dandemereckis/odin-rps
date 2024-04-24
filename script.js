1e11e8d34a4e81202be9aba4a64f365b8632fc35
Dan Demereckis <Dandemereckis@gmail.com>
2024-04-24 17:39:52 -0500

    Created playRound function

--- script.js
+++ script.js
@@ -16,5 +16,36 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+    console.log("Computer wins! Paper beats rock");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    console.log("Player wins! Rock beats scissors");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    console.log("Player wins! Paper beats rock");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
+    console.log("Computer wins! Scissors beats paper");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+    console.log("Computer wins! Rock beats scissors");
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    console.log("Player wins! Scissors beats paper");
+  } else {
+    console.log("Tie, try again!");
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanSelection, computerSelection);
+console.log(humanScore, computerScore);
